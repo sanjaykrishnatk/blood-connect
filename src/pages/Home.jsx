c5ddbc44c40bc1b0910f70d1f8027c7f5e511b22
SANJAY <sktk9909@gmail.com>
2024-07-01 21:12:21 +0530

    landingpage section 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import { Row } from "react-bootstrap";
 function Home() {
   return (
     <>
@@ -17,17 +18,15 @@ function Home() {
             <Navbar.Collapse id="basic-navbar-nav" style={{ width: "50%" }}>
               <Nav className="ms-auto d-flex justify-content-center align-items-center">
                 <Nav.Link href="#home">
-                  <h5 className="text-light fw-bold nav-bar-txt mt-md-2 me-md-2">
-                    HOME
-                  </h5>
+                  <h5 className=" fw-bold nav-bar-txt mt-md-2 me-md-2">HOME</h5>
                 </Nav.Link>
                 <Nav.Link href="#link">
-                  <h5 className="text-light fw-bold nav-bar-txt mt-md-2 me-md-2">
+                  <h5 className=" fw-bold nav-bar-txt mt-md-2 me-md-2">
                     ABOUT US
                   </h5>
                 </Nav.Link>
                 <Nav.Link href="#link">
-                  <h5 className="text-light fw-bold nav-bar-txt mt-md-2 me-md-2">
+                  <h5 className=" fw-bold nav-bar-txt mt-md-2 me-md-2">
                     SERVICES
                   </h5>
                 </Nav.Link>
@@ -45,6 +44,30 @@ function Home() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        <Row className="ms-0 me-0 w-100 d-flex flex-column pb-5">
+          <h1 className=" landing-header mb-0" style={{ marginTop: "100px" }}>
+            <span style={{ color: "#D70012" }}>hope</span> starts
+            <br />
+            with a <span style={{ color: "#D70012" }}>drop</span>
+          </h1>
+          <h4 className="mb-0 fw-bold tagline">
+            Be the Reason Someone Smiles Today
+          </h4>
+          <Row className="me-0 ms-0 d-flex mt-3 landing-btns">
+            <Button
+              variant="light fw-bold rounded-1 p-2"
+              style={{ width: "125px" }}
+            >
+              Get Started
+            </Button>{" "}
+            <Button
+              variant="danger fw-bold rounded-1 ms-3 p-2"
+              style={{ width: "125px" }}
+            >
+              Donate Now
+            </Button>{" "}
+          </Row>
+        </Row>
       </section>
     </>
   );
