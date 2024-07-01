c96773a2bd0256f87fc2416e2a88910fe6d2f1de
SANJAY <sktk9909@gmail.com>
2024-07-01 16:42:18 +0530

    home page changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
