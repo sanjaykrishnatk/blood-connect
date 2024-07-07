3d3ceaed75618f19c526059b3dca4df5d0ed20b6
SANJAY <sktk9909@gmail.com>
2024-07-07 10:56:54 +0530

    file changes

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,7 +4,7 @@ import { BarChart } from "@mui/x-charts/BarChart";
 import { axisClasses } from "@mui/x-charts/ChartsAxis";
 import { PieChart } from "@mui/x-charts/PieChart";
 import Table from "../components/Table";
-import './Admin.css'
+import "./Admin.css";
 function Admin() {
   const chartSetting = {
     yAxis: [
@@ -113,25 +113,27 @@ function Admin() {
               ]}
               {...chartSetting}
             />
-           
-           
-              <PieChart
-                series={[
-                  {
-                    data: [
-                      { id: 0, value: 10, label: "series A" },
-                      { id: 1, value: 15, label: "series B" },
-                      { id: 2, value: 20, label: "series C" },
-                    ],
-                  },
-                ]}
-                width={400}
-                height={200}
-              />
-            </div>
-          
+
+            <PieChart
+              series={[
+                {
+                  data: [
+                    { id: 0, value: 10, label: "series A" },
+                    { id: 1, value: 15, label: "series B" },
+                    { id: 2, value: 20, label: "series C" },
+                  ],
+                },
+              ]}
+              width={400}
+              height={200}
+            />
+          </div>
+
           <div className="row w-100 table">
-           <table className="table-responsive"> <Table /></table>
+            <table className="table-responsive">
+              {" "}
+              <Table />
+            </table>
           </div>
         </div>
       </div>
