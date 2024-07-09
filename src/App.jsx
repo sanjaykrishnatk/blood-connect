09d91c148849eb1b6bf0a35e6d1c5d61dff64b7c
SANJAY <sktk9909@gmail.com>
2024-07-09 16:33:31 +0530

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,27 @@ import Request from "./pages/Request";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Donorpage from "./pages/Donorpage";
 import Donorhistory from "./pages/Donorhistory";
-<<<<<<< HEAD
-import Footer from "./components/Footer"
-=======
+import Footer from "./components/Footer";
 import UserDashboard from "./components/UserDashboard";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import DonorDashboard from "./components/DonorDasboard";
-
-
->>>>>>> 36b3e4600c720aff1474ddf73cf469aee1ea003c
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-         <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Reportbloodrequest" element={<Reportbloodrequest />} />
         <Route path="/Reportdonor" element={<Reportdonor />} />
         {/* <Route path="/donorpage" element={<Donorpage/>} /> */}
-        <Route path="/donorhistory" element={<Donorhistory/>} />
+        <Route path="/donorhistory" element={<Donorhistory />} />
         <Route path="/UserDashboard" element={<UserDashboard />} />
-        <Route path="/donorpage" element={<DonorDashboard/>} />
+        <Route path="/donorpage" element={<DonorDashboard />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
