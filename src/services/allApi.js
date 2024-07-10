35a26d5ae85deaba07c93062f3a550c59a2ae069
Bhagyas12 <bhagyasctk@gmail.com>
2024-07-10 17:09:52 +0530

    api added donopage and donorhistory

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -20,3 +20,20 @@ export const getDonorsByBloodGroupApi = async (bloodGroup) => {
 export const retrieveRequestApi = async () => {
   return await commonApi("GET", `${serverUrl}/requests`, "");
 };
+
+
+// API to retrieve last donation date by donor ID
+export const retrieveLastDonation = async (donorId) => {
+  return await commonApi("GET", `${serverUrl}/donors/${donorId}`, "");
+};
+
+// API to update last donation date by donor ID
+export const updateLastDonationApi = async (donorId, reqBody) => {
+  return await commonApi("PATCH", `${serverUrl}/donors/${donorId}`, reqBody);
+};
+
+// API to retrieve history to donorhistory
+export const retrieveHistoryApi = async () => {
+  return await commonApi("GET", `${serverUrl}/history`, "");
+};
+
