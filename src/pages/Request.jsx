14f354413549f7ace3de0043af6eee16827c374a
SANJAY <sktk9909@gmail.com>
2024-07-11 21:52:15 +0530

    sms configuration

--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -92,249 +92,245 @@ function Request() {
       });
 
       if (result.status >= 200 && result.status < 300) {
+        const rid = result.data.id;
         handleClose();
-        if (result.status >= 200 && result.status < 300) {
-          const rid = result.data.id;
-          handleClose();
-          toast.success("Form Submitted Successfully");
-          const donorDetails = await getDonorsByBloodGroupApi(bloodGroup);
-          console.log(donorDetails.data);
-          const eligibleDonors = donorDetails.data.filter((donor) =>
-            atleastThreeMonths(donor.lastDonation, formattedStartDate)
-          );
-          console.log(eligibleDonors);
-          const mobileNumbers = eligibleDonors
-            .map((item) => item.phone)
-            .toString();
-          console.log(mobileNumbers);
-          await Promise.all(
-            eligibleDonors.map(async (item) => {
-              let message = {
-                route: "q",
-                message: `Hi ${item?.username}, An urgent blood donation request matches your profile. Your help can save a life! Click here for details and to confirm your donation: https://blood-connect-seven.vercel.app/?rid=${rid}&did=${item.id}
+        toast.success("Form Submitted Successfully");
+        const donorDetails = await getDonorsByBloodGroupApi(bloodGroup);
+        console.log(donorDetails.data);
+        const eligibleDonors = donorDetails.data.filter((donor) =>
+          atleastThreeMonths(donor.lastDonation, formattedStartDate)
+        );
+        console.log(eligibleDonors);
+        const mobileNumbers = eligibleDonors
+          .map((item) => item.phone)
+          .toString();
+        console.log(mobileNumbers);
+        await Promise.all(
+          eligibleDonors.map(async (item) => {
+            let message = {
+              route: "q",
+              message: `Hi ${item?.username}, An urgent blood donation request matches your profile. Your help can save a life! Click here for details and to confirm your donation: https://blood-connect-seven.vercel.app/?rid=${rid}&did=${item.id}
         
 Thank you,
 BloodConnect Team`,
-                flash: 0,
-                numbers: item?.phone,
-              };
-              const smsResponse = await smsApi(message);
-              console.log(smsResponse.data);
-            })
-          );
-        } else {
-          toast.error("Something went wrong");
-        }
+              flash: 0,
+              numbers: item?.phone,
+            };
+            const smsResponse = await smsApi(message);
+            console.log(smsResponse.data);
+          })
+        );
+      } else {
+        toast.error("Something went wrong");
       }
     }
+  };
 
-    return (
-      <>
-        <div className=" mt-5 mb-5 d-flex justify-content-center align-items-center">
-          <div className="bg-light p-5 rounded">
-            <h1 className="text-danger">Blood Request Form</h1>
+  return (
+    <>
+      <div className=" mt-5 mb-5 d-flex justify-content-center align-items-center">
+        <div className="bg-light p-5 rounded">
+          <h1 className="text-danger">Blood Request Form</h1>
 
-            <Form>
-              <Row className="mb-5">
-                <Form.Group as={Col} controlId="username">
-                  <Form.Label>Pateint's Name : </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter your Name"
-                    value={bloodRequest.userName}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, userName: value });
-                    }}
-                  />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="age">
-                  <Form.Label>Age :</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder=" "
-                    value={bloodRequest.age}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, age: value });
-                    }}
-                  />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="gender">
-                  <Form.Label>Gender : </Form.Label>
-
-                  {["radio"].map((type) => (
-                    <div key={`inline-${type}`} className="mb-3">
-                      <Form.Check
-                        inline
-                        label="male"
-                        name="group1"
-                        type={type}
-                        id={`inline-${type}-male`}
-                        value="male"
-                        checked={bloodRequest.gender === "male"}
-                        onChange={(event) => {
-                          const value = event.target.value;
-                          console.log(value);
-                          setBloodRequest({ ...bloodRequest, gender: value });
-                        }}
-                      />
-                      <Form.Check
-                        inline
-                        label="female"
-                        name="group1"
-                        type={type}
-                        id={`inline-${type}-female`}
-                        value="female"
-                        checked={bloodRequest.gender === "female"}
-                        onChange={(event) => {
-                          const value = event.target.value;
-                          console.log(value);
-                          setBloodRequest({ ...bloodRequest, gender: value });
-                        }}
-                      />
-                      <Form.Check
-                        inline
-                        label="other"
-                        name="group1"
-                        type={type}
-                        id={`inline-${type}-other`}
-                        value="other"
-                        checked={bloodRequest.gender === "other"}
-                        onChange={(event) => {
-                          const value = event.target.value;
-                          console.log(value);
-                          setBloodRequest({ ...bloodRequest, gender: value });
-                        }}
-                      />
-                    </div>
-                  ))}
-                </Form.Group>
-              </Row>
-
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="bloodgrp">
-                  <Form.Label>Blood Group : </Form.Label>
-                  <Form.Select
-                    aria-label="Blood Group"
-                    value={bloodRequest.bloodGroup}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, bloodGroup: value });
-                    }}
-                  >
-                    <option>Select...</option>
-                    {bloodGroupOptions.map((option) => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </Form.Select>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="date">
-                  <Form.Label>Blood Required Date : </Form.Label>
-                  <DatePicker
-                    selected={bloodRequest.startDate}
-                    // onChange={(date) => setBloodRequest({ ...bloodRequest, startDate: date.toLocaleDateString() })}
-                    onChange={(date) =>
-                      setBloodRequest({ ...bloodRequest, startDate: date })
-                    }
-                    dateFormat="dd/MM/yyyy"
-                  />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="unit">
-                  <Form.Label>Units required :</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder="Units"
-                    value={bloodRequest.unit}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, unit: value });
-                    }}
-                  />
-                </Form.Group>
-              </Row>
-
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="state">
-                  <Form.Label>State :</Form.Label>
-                  <Form.Select
-                    value={bloodRequest.state}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, state: value });
-                    }}
-                  >
-                    <option>Select...</option>
-                    {stateOptions.map((option) => (
-                      <option key={option.key} value={option.name}>
-                        {option.name}
-                      </option>
-                    ))}{" "}
-                  </Form.Select>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="district">
-                  <Form.Label>District :</Form.Label>
-                  <Form.Control
-                    value={bloodRequest.district}
-                    onChange={(event) => {
-                      const value = event.target.value;
-                      console.log(value);
-                      setBloodRequest({ ...bloodRequest, district: value });
-                    }}
-                  />
-                </Form.Group>
-              </Row>
-
-              <Form.Group className="mb-3" controlId="phone">
-                <Form.Label>Phone Number : </Form.Label>
+          <Form>
+            <Row className="mb-5">
+              <Form.Group as={Col} controlId="username">
+                <Form.Label>Patient's Name : </Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder=""
-                  value={bloodRequest.phone}
+                  placeholder="Enter your Name"
+                  value={bloodRequest.userName}
                   onChange={(event) => {
                     const value = event.target.value;
                     console.log(value);
-                    setBloodRequest({ ...bloodRequest, phone: value });
+                    setBloodRequest({ ...bloodRequest, userName: value });
                   }}
                 />
               </Form.Group>
 
-              <div className="d-flex justify-content-between">
-                <Button
-                  className="ms-3 fs-5"
-                  variant="danger"
-                  type="submit"
-                  onClick={handleSubmit}
-                >
-                  Submit
-                </Button>
+              <Form.Group as={Col} controlId="age">
+                <Form.Label>Age :</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder=" "
+                  value={bloodRequest.age}
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    console.log(value);
+                    setBloodRequest({ ...bloodRequest, age: value });
+                  }}
+                />
+              </Form.Group>
 
-                <Button
-                  className="ms-5 fs-5"
-                  variant="warning"
-                  onClick={handleClose}
+              <Form.Group as={Col} controlId="gender">
+                <Form.Label>Gender : </Form.Label>
+
+                {["radio"].map((type) => (
+                  <div key={`inline-${type}`} className="mb-3">
+                    <Form.Check
+                      inline
+                      label="male"
+                      name="group1"
+                      type={type}
+                      id={`inline-${type}-male`}
+                      value="male"
+                      checked={bloodRequest.gender === "male"}
+                      onChange={(event) => {
+                        const value = event.target.value;
+                        console.log(value);
+                        setBloodRequest({ ...bloodRequest, gender: value });
+                      }}
+                    />
+                    <Form.Check
+                      inline
+                      label="female"
+                      name="group1"
+                      type={type}
+                      id={`inline-${type}-female`}
+                      value="female"
+                      checked={bloodRequest.gender === "female"}
+                      onChange={(event) => {
+                        const value = event.target.value;
+                        console.log(value);
+                        setBloodRequest({ ...bloodRequest, gender: value });
+                      }}
+                    />
+                    <Form.Check
+                      inline
+                      label="other"
+                      name="group1"
+                      type={type}
+                      id={`inline-${type}-other`}
+                      value="other"
+                      checked={bloodRequest.gender === "other"}
+                      onChange={(event) => {
+                        const value = event.target.value;
+                        console.log(value);
+                        setBloodRequest({ ...bloodRequest, gender: value });
+                      }}
+                    />
+                  </div>
+                ))}
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="bloodgrp">
+                <Form.Label>Blood Group : </Form.Label>
+                <Form.Select
+                  aria-label="Blood Group"
+                  value={bloodRequest.bloodGroup}
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    console.log(value);
+                    setBloodRequest({ ...bloodRequest, bloodGroup: value });
+                  }}
                 >
-                  Reset
-                </Button>
-              </div>
-            </Form>
-          </div>
+                  <option>Select...</option>
+                  {bloodGroupOptions.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="date">
+                <Form.Label>Blood Required Date : </Form.Label>
+                <DatePicker
+                  selected={bloodRequest.startDate}
+                  onChange={(date) =>
+                    setBloodRequest({ ...bloodRequest, startDate: date })
+                  }
+                  dateFormat="dd/MM/yyyy"
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="unit">
+                <Form.Label>Units required :</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Units"
+                  value={bloodRequest.unit}
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    console.log(value);
+                    setBloodRequest({ ...bloodRequest, unit: value });
+                  }}
+                />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="state">
+                <Form.Label>State :</Form.Label>
+                <Form.Select
+                  value={bloodRequest.state}
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    console.log(value);
+                    setBloodRequest({ ...bloodRequest, state: value });
+                  }}
+                >
+                  <option>Select...</option>
+                  {stateOptions.map((option) => (
+                    <option key={option.key} value={option.name}>
+                      {option.name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="district">
+                <Form.Label>District :</Form.Label>
+                <Form.Control
+                  value={bloodRequest.district}
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    console.log(value);
+                    setBloodRequest({ ...bloodRequest, district: value });
+                  }}
+                />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="phone">
+              <Form.Label>Phone Number : </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                value={bloodRequest.phone}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  console.log(value);
+                  setBloodRequest({ ...bloodRequest, phone: value });
+                }}
+              />
+            </Form.Group>
+
+            <div className="d-flex justify-content-between">
+              <Button
+                className="ms-3 fs-5"
+                variant="danger"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+
+              <Button
+                className="ms-5 fs-5"
+                variant="warning"
+                onClick={handleClose}
+              >
+                Reset
+              </Button>
+            </div>
+          </Form>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 }
 
 export default Request;
